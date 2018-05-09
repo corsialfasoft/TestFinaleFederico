e0d3b51cc30763063f993e4da4cc3ede6a4652d9
FedericoMarras <justoxion@gmail.com>
2018-05-09 14:14:15 +0200

    Esercizio 1 Creato, Completato

---
+++ PrimoEsericioz/PrimoEsericioz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimoEsericioz {
+	class Program {
+		static void Main(string[] args) {
+
+
+		for(int i = 1; i <= 15; i++) {
+		int pari;
+		int numero = 2;
+
+		pari = i * numero;
+		Console.WriteLine(pari);
+		
+		}
+
+		for(int j = 30 ; j >= 1  ;j-- ){
+		
+			if(j%2!=0){ 
+			Console.WriteLine(j); 
+				} 		
+		
+		}
+
+
+
+		}
+	}
+}
